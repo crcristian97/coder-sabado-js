c68b131fe7ae7c87eb0d208ab256718d20f3764b
crcristian97 <crcristian97.cc@gmail.com>
2025-03-01 13:19:52 -0300

    clase 03

--- Clase03/JS/codeLive.js
+++ Clase03/JS/codeLive.js
@@ -4,7 +4,7 @@ Si tenemos un negocio y todos los meses compramos productos para vender pero por
 Teniendo la constante IVA = 1.21, imagina una función flecha la cual reciba un valor numérico como parámetro, 
 y que retorne dicho valor con IVA incluido. 
 El IVA debe tomarse de una constante, para que la función sea dinámica. ¿Cómo te imaginas el resultado?
-*/
+
 
 const IVA = 1.21;
 const precioUnitario = parseInt(prompt('Decime el precio de tu producto'));
@@ -17,3 +17,31 @@ const productoIVA = (precioUnitario, IVA) => {
 console.log("El iva es: ", IVA);
 console.log("El precio del producto sin IVA es: ",precioUnitario)
 console.log(productoIVA(precioUnitario, IVA));
+*/
+
+
+const IVA = 1.21;
+let importe = prompt("Ingresa el importe sobre el cual quieres calcular el IVA:");
+
+const precioConIVA = ( imp, iva ) => imp * iva;
+
+precioConIVA(importe, IVA);
+
+//¿Qué tiene que hacer este código?
+//¿Por qué no cumple con su función?
+
+//Actividad 02:
+const valorM2 = 31.83; //valor fijo del seguro por Metro 2
+const comision = 1.025; //comisión del 2.5%
+
+const M2 = prompt("Ingresa los Metros cuadrados de la propiedad acotizar un seguro:");
+
+const calcularPoliza = m2 => { m2 * valorM2 * comision };
+
+const valorPoliza = calcularPoliza(M2);
+
+console.log("El costo de la póliza es: $", valorPoliza);
+
+//¿Qué tiene que hacer este código?
+//¿Por qué no cumple con su función?
+//¿Qué propuesta podrías hacer para que tenga sentido su uso?
